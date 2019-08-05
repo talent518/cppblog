367e911ee26c1fb0757e0a2b663ab7e8c8cdf443
ZhangBaoCai <talent518@yeah.net>
2019-08-05 11:19:55 +0800

    优化分页。

--- data/basic_master.cpp
+++ data/basic_master.cpp
@@ -4,24 +4,26 @@
 
 namespace data {
 	#define MULTI_PAGE(url) \
-		if(page_total <= 1) return ""; \
-		\
-		int b = curpage - 5, e; \
 		std::ostringstream ss; \
-		if(b < 0) b = 0; \
-		\
-		e = b + 9; \
-		if(e >= page_total) e = page_total -1; \
-		\
 		ss << "<div class=\"multi-page\">" << std::endl; \
 		\
 		ss << "<span class=\"desc\">" << cppcms::locale::format(app().translate("Total {1} pages, {2} records.")) % (cppcms::filters::escape(page_total)) % (cppcms::filters::escape(page_records)) << "</span>" << std::endl; \
 		\
-		for(;b<=e;b++) { \
-			if(b == curpage) \
-				ss << "<span class=\"page cur\">" << b+1 << "</span>" << std::endl; \
-			else \
-				ss << "<a href=\"" << url << "\" class=\"page\">" << b+1 << "</a>" << std::endl; \
+		if(page_total > 1) { \
+			int b = curpage - 5, e; \
+			if(b < 0) b = 0; \
+			\
+			e = b + 9; \
+			if(e >= page_total) e = page_total -1; \
+			\
+			for(;b<=e;b++) { \
+				if(b == curpage) \
+					ss << "<span class=\"page cur\">" << b+1 << "</span>" << std::endl; \
+				else \
+					ss << "<a href=\"" << url << "\" class=\"page\">" << b+1 << "</a>" << std::endl; \
+			} \
+		} else { \
+			ss << "<span class=\"page cur\">1</span>" << std::endl; \
 		} \
 		\
 		ss << "</div>"; \
